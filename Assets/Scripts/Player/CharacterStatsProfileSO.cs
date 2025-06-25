37cfc2d94e41f67c3c3be47f8b4fbbb0e6b1c41e
Valentin Gaudio <valentin.gaudio@7r1ck.com>
2025-06-24 21:28:21 -0300

    Stat System

---
+++ Assets/Scripts/Player/CharacterStatsProfileSO.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Characters/Stat Profile")]
+public class CharacterStatsProfileSO : ScriptableObject
+{
+    [System.Serializable]
+    public struct StatEntry
+    {
+        public StatDefinitionSO stat;
+        public int baseValue;
+    }
+
+    public List<StatEntry> stats;
+}
---
+++ Assets/Scripts/Player/StatType.cs
@@ -0,0 +1,10 @@
+public enum StatType
+{
+    MaxHealth,
+    Strength,
+    Defense,
+    Speed,
+    MagicPower,
+    AttackSpeed,
+    CritChance
+}
